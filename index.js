0ae83b4f0b31141826967d432802f27bc1412c28
Philip Dutra <monsterphilip10@gmail.com>
2023-02-27 23:25:10 -0500

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1")
+document.body.append(newHeader);
+newHeader.id = "victory"
+newHeader.innerHTML = "Phil is the champion"
