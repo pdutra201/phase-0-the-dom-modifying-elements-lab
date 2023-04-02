9ef51d968c588afc828287a8f79566a4f49219d2
philip dutra <monsterphilip10@gmail.com>
2023-04-02 16:52:55 -0400

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const element = document.getElementById("main");
 element.remove();
 
-const newHeader = document.createElement("h1")
-document.body.append(newHeader);
-newHeader.id = "victory"
-newHeader.innerHTML = "Phil is the champion"
+const newHeader = document.createElement("h1");
+
+//document.body.append(newHeader);
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Phil is the champion";
